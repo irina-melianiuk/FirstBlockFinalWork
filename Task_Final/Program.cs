5781fa8c75f9ba3c8ebed061421e357af2c139db
Irina Melianiuk <ira87m@mail.ru>
2023-03-23 09:02:45 +0300

    Добавили описание решения задачи

--- Task_Final/Program.cs
+++ Task_Final/Program.cs
@@ -33,7 +33,7 @@ void PrintArray(string[] arr)
   Console.Write("[");
   for (int i = 0; i < arr.Length - 1; i++)
   {
-    Console.Write($"{arr[i]}  ");
+    Console.Write($"{arr[i]} ");
   }
   Console.Write($"{arr[arr.Length - 1]}");
   Console.Write("]");
